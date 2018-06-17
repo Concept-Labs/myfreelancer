2c3e23d823e0d5639f9a7743be352459b2535ca3
StingerWin <witalikwin@gmail.com>
2018-06-17 15:41:43 +0300

    пав

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$dbhost = 'localhost';
-	$dbname = 'test';
-	$dbuser = 'root';
+	$dbname = '';
+	$dbuser = '';
 	$dbpass = '';
 	
 ?>
--- controllers/index.php
+++ controllers/index.php
@@ -6,20 +6,37 @@ Class Controller_Index Extends Controller_Base
         $this->_baseTemplate->addCss('styles/main.css');
 		//єто файл templates/index.phtml
         return parent::_initTemplate($title);
-      
-	}
+
+    }
 
     public function index() 
     {
-     	
-		$template = $this->_initTemplate('MY FREELANCER');
-        
-        $template->set("data", "Hello", false);
-        $template->set("name", "Vitalik", false);
-        $template->set("test", "<a href='tree'>Go to tree</a>", true);
-        $template->set("linkName", "Google.com");
-        $template->setFile('templates/main.phtml');
-        
-        $this->_renderLayout($template);
+
+      $template = $this->_initTemplate('MY FREELANCER');
+
+      $template->setFile('templates/main.phtml');
+
+      $data = $_POST;
+      if (isset($data['send'])) {
+        $to = $data['email'];
+        $subject = 'Здраствуйте '.$data['name'].'!';
+        $message = $data['coment'];
+        $headers = 'From:'.$to . "\r\n" .
+        'Reply-To:'.$to  . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+         if (!empty($_POST['email']) AND !empty($_POST['name']) AND !empty($_POST['coment'])) {
+        if ($mail = mail($to, $subject, $message, $headers)) {
+            echo "повідомлення відправлено".$mail;
+
+        } else {
+            echo "повідомлення не відправлено";
+        }
+    }else {
+        echo "нема даних";
     }
+    }
+
+    $this->_renderLayout($template);
+}
 }
